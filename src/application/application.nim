f057d9f39d956e9bbb648464322f7d2f520a5f04
koiralasanskrit <sanskritdarshan@gmail.com>
2023-03-28 09:27:05 +0545

    not

--- src/application/application.nim
+++ src/application/application.nim
@@ -3,7 +3,7 @@ import ../nim_glpkg/glm as m
 import shaders as s
 import ../nim_glpkg/assimp as a
 import std/macros
-
+import models as models
 
 
 var vao: vao
@@ -20,41 +20,13 @@ macro new_carray(typo: untyped, vari: untyped, to: untyped, size: untyped): unty
         var `lit` : cint = (cint)`size`
 
 
-var indices_to_be_drawn : cint
+var theobj : models.obj
 proc init*() : void = 
     var sceneptr : ptr aiScene
-    var mode : ptr model
-    var vert : ptr vertex
     sceneptr = import_file("res/test.obj", sceneptr)
+    get_models(sceneptr, theobj.model.addr)
+    theobj.init(1)
 
-    get_models(sceneptr, mode.addr)
-    model_to_vertex(mode, vert.addr, 1)
-
-    new_carray(model, models, mode, 3)
-
-    new_carray(aiMesh, meshes, sceneptr[].mMeshes[], sceneptr[].mNumMeshes)
-    new_carray(aiVector3D, vertix, meshes[0].mVertices, meshes[0].mNumVertices)
-    new_carray(aiFace, faces, meshes[0].mFaces, meshes[0].mNumFaces)
-
-    var indix : seq[cuint]
-    indices_to_be_drawn = (cint)models[1].ntris
-
-    
-    for i in countup(0, faces_size - 1):
-        new_carray(cuint, axx, faces[i].mIndices, faces[i].mNumIndices)
-        for j in countup(0, axx_size - 1):
-            indix.add(axx[j])
-
-    vao.init()
-    vbl.init()
-    vbl.push(3)
-    vbl.push(3)
-    vbl.push(3)
-    ibo.init(data=models[1].tris, size=(cuint)(models[1].ntris * (cuint)sizeof(tris)), types=k.GL_DYNAMIC_DRAW)
-    #vbo.init(data=models[1].pos, size=(cuint)(models[1].npos * (cuint)sizeof(position)), types=k.GL_DYNAMIC_DRAW)
-    vbo.init(data=vert, size=(cuint)(models[1].npos * (cuint)sizeof(vertex)), types=k.GL_DYNAMIC_DRAW)
-    vao.add_buffer(vbo, vbl)
-    pro = compile_shaders(s.vnormal_fill(), s.fnormal_fill())
 
 proc draw*() = 
     var eye : vec3 = [(cfloat)3, (cfloat)3, (cfloat) 3]
@@ -63,9 +35,9 @@ proc draw*() =
     var look_at : mat4 = m.lookat(eye, center, up)
     var perspective : mat4 = m.persp(0.7853, 640/480, 0.1, 100)
     var mvp : mat4 = perspective * look_at
-    pro.bind_shader()
-    k.uniform_mat4(pro, "u_mvp", mvp[0][0].unsafeAddr)
-    pro.bind_shader()
-    k.draw_elements(indices_to_be_drawn * 3, nil)
+    theobj.pro.bind_shader()
+    k.uniform_mat4(theobj.pro, "u_mvp", mvp[0][0].unsafeAddr)
+    theobj.pro.bind_shader()
+    k.draw_elements((cint)theobj.ntris * 3, nil)
 
 proc destroy*() = discard
---
+++ src/application/models.nim
@@ -0,0 +1,49 @@
+import ../nim_glpkg/ksa as k
+import ../nim_glpkg/glm as m
+import ../nim_glpkg/assimp as a
+import shaders as s
+import std/macros
+
+#[
+    MACROS
+]#
+
+macro new_carray(typo: untyped, vari: untyped, to: untyped, size: untyped): untyped =
+    let lit = ident($`vari` & "_size")
+    quote do:
+        var `vari` : ptr UncheckedArray[`typo`] = cast[ptr UncheckedArray[`typo`]](`to`)
+        var `lit` : cint = (cint)`size`
+
+type
+    obj* = object
+        name* : cstring
+        path* : cstring
+        parent* : ptr obj
+        vao*: vao
+        vbo*: vbo
+        vbl*: vbl
+        pro*: cuint
+        ibo*: ibo
+        vert* : ptr vertex
+        model* : ptr model
+        ntris* : cuint
+
+proc init*(antah: var obj, index: cuint) = 
+    model_to_vertex(antah.model, antah.vert.addr, index)
+    new_carray(model, models, antah.model, index)
+
+    antah.ntris = models[index].ntris
+    antah.vao.init()
+    antah.vbl.init()
+    antah.vbl.push(3)
+    antah.vbl.push(3)
+    antah.vbl.push(3)
+    antah.ibo.init(data=models[index].tris, size=(cuint)(models[index].ntris * (cuint)sizeof(tris)), types=k.GL_DYNAMIC_DRAW)
+    antah.vbo.init(data=nil, size=(cuint)(models[index].npos * (cuint)sizeof(vertex)), types=k.GL_DYNAMIC_DRAW)
+    antah.vao.add_buffer(antah.vbo, antah.vbl)
+    antah.pro = compile_shaders(s.vnormal_fill(), s.fnormal_fill())
+
+proc draw*(antah: obj) =
+    k.draw_elements((cint)antah.model[].ntris * 3, nil)
+
+proc destroy*(antah: obj) = discard
